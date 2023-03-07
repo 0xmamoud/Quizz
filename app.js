1a2ab10f9e882e593274069f6ad5c2c9209cb5fb
Kane <mamad2k18@gmail.com>
2023-03-07 14:41:27 +0100

    maj

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+
+
+const form = document.querySelector(".quiz-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e){
+    e.preventDefault();
+}
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+const inputsTab = Array.from(inputs);
+
+for (let input of inputsTab){
+    console.log(inputsTab)
+}
